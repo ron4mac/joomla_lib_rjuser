1c1dca332a9ecce1256bfe7fa0e9012efad046df
ron4mac <ron@rjconline.net>
2025-05-22 11:37:40 +0000

    update for J5

--- src/RJUserCom.php
+++ src/RJUserCom.php
@@ -3,16 +3,25 @@
 * @package		lib_rjuser
 * @copyright	Copyright (C) 2022-2024 RJCreations. All rights reserved.
 * @license		GNU General Public License version 3 or later; see LICENSE.txt
-* @since		1.3.2
+* @since		1.3.5
 */
+namespace RJCreations\Library;
 
 use Joomla\CMS\Factory;
 use Joomla\Database\DatabaseDriver;
+use Joomla\CMS\Application\ApplicationHelper;
 
 abstract class RJUserCom
 {
 	protected static $siteMenu = null;
 	protected static $instObj = null;
+
+	// sanity check for components needing certain requirement
+	public static function Igaa ()
+	{
+		return 4;
+	}
+
 
 	public static function shout ()
 	{
@@ -20,7 +29,7 @@ abstract class RJUserCom
 	}
 
 
-	public static function getInstObject ($ityp, $mid=null)	// SO
+	public static function getInstObject ($mid=null)	// SO
 	{
 		if (self::$instObj) return self::$instObj;
 
@@ -31,53 +40,66 @@ abstract class RJUserCom
 		} else {
 			$params = $app->getParams();
 			$menuid = $app->input->getInt('Itemid', 0);
-		}
-		if (!$menuid) throw new Exception('NOT ALLOWED: missing menu ID', 400);
+		}	//echo'<pre>';var_dump($mid,$params);echo'</pre>';
+		if (!$menuid) throw new \Exception('NOT ALLOWED: missing menu ID', 400);
 		$user = $app->getIdentity();
 		$uid = $user->get('id');
 		$ugrps = $user->get('groups');
 		$allperms = RJUserInstanceObject::CAN_CREA + RJUserInstanceObject::CAN_EDIT + RJUserInstanceObject::CAN_DELE;
+		$ityp = $params->get('instance_type');
 		$path = '';
 		$perms = 0;
-		switch ($params->get($ityp)) {
+		switch ($ityp) {
 			case 0:	//user
 				if ($uid) $perms = $allperms;
 				$path = '@'.$uid;
 				break;
 			case 1:	//group
-				$auth = $params->get('group_auth');
-				$path = '_'.$auth;
-				if ($uid && in_array($auth, $ugrps)) $perms = $allperms;
+				//$auth = $params->get('group_auth');
+				$auth = $params->get('group_admin', []);
+				//$path = '_'.$auth;
+				$path = '_'.$params->get('group_owner', 123456789);
+				if ($uid && array_intersect($ugrps, $auth)) $perms = $allperms;
 				break;
 			case 2:	//site
-				$auth = $params->get('site_auth');
+				$auth = $params->get('site_admin', []);
 				$path = '_0';
-				if ($uid && in_array($auth, $ugrps)) $perms = $allperms;
+				if ($uid && array_intersect($ugrps, $auth)) $perms = $allperms;
 				break;
 		}
-		self::$instObj = new RJUserInstanceObject($params->get($ityp), $menuid, $uid, $path, $perms);
+		self::$instObj = new RJUserInstanceObject($ityp, $menuid, $uid, $path, $perms);
+		//echo'<pre>';var_dump($ugrps,$auth,self::$instObj);echo'</pre>';
 		return self::$instObj;
 	}
 
 
-	public static function userDataPath ($instObj)
+	public static function getStorageBase ()
 	{
-		$cmp = JApplicationHelper::getComponentName().'_'.$instObj->menuid;
-		return self::getStorPath().'/'.$instObj->path.'/'.$cmp;
+		$results = Factory::getApplication()->triggerEvent('onRjuserDatapath');
+		$sb = trim($results[0] ?? '');
+		return ($sb ?: 'userstor');
 	}
 
 
-	public static function getStoragePath ($instObj)
+	public static function userDataPath ($instObj=null)
 	{
-		$cmp = JApplicationHelper::getComponentName().'_'.$instObj->menuid;
-		return self::getStorPath().'/'.$instObj->path.'/'.$cmp;
+		// alias
+		return self::getStoragePath($instObj);
+	}
+
+
+	public static function getStoragePath ($instObj=null)
+	{
+		if (!$instObj) $instObj = self::getInstObject();
+		$cmp = ApplicationHelper::getComponentName().'_'.$instObj->menuid;
+		return self::getStorageBase().'/'.$instObj->path.'/'.$cmp;
 	}
 
 
 	public static function getDbPaths ($which, $dbname, $full=false, $cmp='')	// AO
 	{
 		$paths = [];
-		if (!$cmp) $cmp = JApplicationHelper::getComponentName();
+		if (!$cmp) $cmp = ApplicationHelper::getComponentName();
 		$cmp_ = $cmp.'_';
 		$cmpl = strlen($cmp_);
 		switch ($which) {
@@ -94,7 +116,7 @@ abstract class RJUserCom
 				$char1 = '@_';
 				break;
 		}
-		$dpath = JPATH_SITE.'/'.self::getStorPath().'/';
+		$dpath = JPATH_SITE.'/'.self::getStorageBase().'/';
 		if (is_dir($dpath) && ($dh = opendir($dpath))) {
 			if (!self::$siteMenu) {
 				self::$siteMenu = Factory::getApplication()->getMenu('site');
@@ -109,10 +131,12 @@ abstract class RJUserCom
 							$mnut = 'OLD STORAGE LOCATION SCHEMA';
 						} elseif (substr($dir,0,$cmpl)==$cmp_) {
 							$mnu = (int)substr($dir,$cmpl);			//echo'<xmp>';var_dump(self::$siteMenu->getItem($mnu));echo'</xmp>';
-							$mnut = self::$siteMenu->getItem($mnu)->title." ({$mnu})";
+							$mob = self::$siteMenu->getItem($mnu);
+							$mnut = ($mob ? $mob->title : '??')." ({$mnu})";
 						} else echo "$cmpl $cmp_ $dir<br>";
 						$ptf = $mid.'/'.$dbname.'.sql3';
 						if (!file_exists($ptf)) $ptf = $mid.'/'.$dbname.'.db3';
+						if (!file_exists($ptf)) $ptf = $mid.'/'.$dbname.'.sql3';
 						if (!file_exists($ptf)) $ptf = $mid.'/'.$dbname.'.sqlite';
 						if (file_exists($ptf)) $paths[$file][$mnu] = ['path'=>$full ? $ptf : $file, 'mnun' => $mnu, 'mnut'=>$mnut];		//$paths[$file] = $full ? $ptf : $file;
 					}
@@ -126,13 +150,13 @@ abstract class RJUserCom
 
 	public static function getDb ($creok=false, $fnam=null, $fext='.db3')
 	{
-		$path = self::getStoragePath(self::getInstObject('instance_type')).'/';
+		$path = self::getStoragePath().'/';
 		$cmpnam = explode('_',basename(JPATH_COMPONENT))[1];
 		$dbfile = $path.($fnam??$cmpnam).$fext;
 //var_dump($dbfile);jexit();
 		if (file_exists($dbfile)) return self::openDb($dbfile);
 //var_dump($dbfile);jexit();
-		if (!$creok) throw new Exception('NOT ALLOWED: create DB', 400);
+		if (!$creok) throw new \Exception('NOT ALLOWED: create DB', 400);
 
 		return self::createDb($path, $fnam, $fext);
 	}
@@ -141,7 +165,7 @@ abstract class RJUserCom
 	public static function createDb ($path, $fnam=null, $fext='.db3')
 	{
 		$cmpnam = explode('_',basename(JPATH_COMPONENT))[1];
-		$dbfile = $path.($fnam??$cmpnam).$fext;
+		$dbfile = $path.'/'.($fnam??$cmpnam).$fext;
 		if (file_exists($dbfile)) return;
 		$db = self::openDb($dbfile);
 		$execs = explode(';', file_get_contents(JPATH_COMPONENT_ADMINISTRATOR.'/sql/'.$cmpnam.'.sql'));
@@ -156,7 +180,7 @@ abstract class RJUserCom
 	public static function getDbInfo ($udbPath, $table, $szcb=null)
 	{
 		if (!file_exists($udbPath)) return [];
-		$size = filesize($udbPath);var_dump(explode('_',basename(JPATH_COMPONENT))[1]);
+		$size = filesize($udbPath);		//var_dump(explode('_',basename(JPATH_COMPONENT))[1]);
 		$db = self::openDb($udbPath);
 		$items = $db->setQuery('SELECT COUNT(*) FROM '.$table)->loadResult();
 		$dbv = $db->setQuery('PRAGMA user_version')->loadResult();
@@ -186,12 +210,12 @@ abstract class RJUserCom
 	}
 
 
-	private static function getStorPath ()
+/*	private static function getStorPath ()
 	{
 		$results = Factory::getApplication()->triggerEvent('onRjuserDatapath');
 		$dsp = trim($results[0] ?? '');
 		return ($dsp ?: 'userstor');
-	}
+	}*/
 
 	private static function openDb ($ptdbf)
 	{
